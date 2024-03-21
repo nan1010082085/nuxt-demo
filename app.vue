35af9890c90d05c18909b2c200f626cb16c120ba
yangdongnan <nan1010082085@163.com>
2024-03-21 13:05:57 +0800

    feat: �init nuxt

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    {{ msg }}
+
+    <UButton>Click Me</UButton>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const msg = ref('Welcome to Nuxt 3!')
+</script>
