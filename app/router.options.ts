cd826c5368787d835758402f6c1f620522001ba2
yangdongnan <nan1010082085@163.com>
2024-04-08 13:37:58 +0800

    fix: 🐛 修复初始路由404，未自动跳转home

---
+++ app/router.options.ts
@@ -0,0 +1,20 @@
+import type { RouterConfig } from '@nuxt/schema';
+
+const pages = ['About', 'Blog', 'Home', 'View'];
+
+export default <RouterConfig>{
+  // https://router.vuejs.org/api/interfaces/routeroptions.html#routes
+  routes: (_routes) => [
+    {
+      path: '/',
+      redirect: '/home'
+    },
+    ...(() => {
+      return pages.map((key) => ({
+        name: key.toLowerCase(),
+        path: `/${key.toLowerCase()}`,
+        component: () => import(`~/pages/${key}.vue`).then((r) => r.default || r)
+      }));
+    })()
+  ]
+};
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   components: true,
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: ['@nuxt/ui', '@pinia/nuxt'],
   modulesDir: ['node_modules']
 });
